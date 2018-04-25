e5afb6236ae8b7f84dcc6d377863f13f4db6c0e0
ArulrajAnto <antopsnec@gmail.com>
2018-04-25 16:18:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var http = require('http');
 http.createServer(function(req, res){
 
 res.writeHead(200, {'content-type':'text/plain'});
-res.end('Hello World! This is Anto test web nodejs page, Will work fine after chh
-anging the page too');
+res.end('Hello World! This is Anto test web nodejs web page');
 }).listen(8080);
 
 console.log('Server is running on a port over 8080 !!');
