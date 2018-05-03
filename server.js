3288fe49bffe2dfdc528395300304ebfe648c1be
ArulrajAnto <antopsnec@gmail.com>
2018-05-03 15:49:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(req, res){
 
 res.writeHead(200, {'content-type':'text/plain'});
-res.end('Hello World! This is Anto test web nodejs web page');
+res.end('Hello World! This is Anto test web nodejs web page with jenkins triger');
 }).listen(8080);
 
 console.log('Server is running on a port over 8080 !!');
